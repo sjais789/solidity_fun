e81896da382fb20770d407450aebf555cf2f5c2b
Shubham <sjais789@users.noreply.github.com>
2018-06-30 14:34:07 +0200

    Update basics.sol

--- basics.sol
+++ basics.sol
@@ -1,3 +1,23 @@
 
 // defining version check latest one!!
 pragma solidity ^0.4.20;  
+contract Scifi {
+
+    uint jupiter= 10;
+    uint mars = 3;
+
+    uint neptune = 7;
+    uint earth = 7;
+
+    uint exponent = 10 ** neptune;
+    uint exponent_it = 10 ** earth;
+
+    struct mars {
+        string nowater;
+        uint8 no_oxygen;
+        uint8 yw_life;
+        uint8 buy_flat;
+        uint8 wanna_go;
+        uint coordX;
+        uint coordY;
+    }
